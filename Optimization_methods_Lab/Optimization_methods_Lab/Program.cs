405c485f4104e1f738281cbe164a86c09d936bd3
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2025-02-17 14:23:50 +0300

    laba1

---
+++ Optimization_methods_Lab/Optimization_methods_Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optimization_methods_Lab
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var menu = new Menu();
+            menu.FormClosed += (sender, args) => Application.Exit();  // Завершаем процесс, когда закрывается главное окно
+            Application.Run(menu);
+        }
+    }
+}
