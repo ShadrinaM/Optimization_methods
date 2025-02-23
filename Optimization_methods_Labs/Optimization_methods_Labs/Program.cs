a08303d0da735bd2ec79c0cec5792b58ba617d27
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2025-02-24 01:06:41 +0300

    аааа

---
+++ Optimization_methods_Labs/Optimization_methods_Labs/Program.cs
@@ -0,0 +1,25 @@
+namespace Optimization_methods_Labs
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            // Устанавливаем режим рендеринга текста
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var menu = new Menu();
+            menu.FormClosed += (sender, args) => Application.Exit();  // Завершаем процесс, когда закрывается главное окно
+            
+            // Запускаем главную форму
+            Application.Run(menu);
+            
+            
+        }
+    }
+}
---
+++ Optimization_methods_Labs/Optimization_methods_Labs/WindowLab1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optimization_methods_Labs
+{
+    public partial class WindowLab1 : Form
+    {
+        private Menu mainForm;
+
+        public WindowLab1(Menu menushka)
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.mainForm = menushka;
+            this.FormClosed += WindowLab1_FormClosed;
+            Lab1();
+
+        }
+        private void BackToMenu_Click(object sender, EventArgs e)
+        {
+            mainForm.Show();
+            this.Close();
+        }
+        private void WindowLab1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            mainForm.Show();
+        }
+
+        private void Lab1()
+        {
+            //лаба
+        }
+    }
+}
