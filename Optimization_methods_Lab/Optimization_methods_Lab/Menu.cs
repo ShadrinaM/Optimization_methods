65cf24204eac41907af00ffb42a9fe5be0fe952d
BusinaLapusina <busina_lapusina@mail.ru>
2025-02-24 01:20:15 +0300

    вава

---
+++ Optimization_methods_Lab/Optimization_methods_Lab/Menu.cs
@@ -0,0 +1,18 @@
+namespace Optimization_methods_Lab
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void StartTask1_Click(object sender, EventArgs e)
+        {
+
+            //WindowLab1 window = new WindowLab1(this);
+            //window.Show();
+            //this.Hide();
+        }
+    }
+}
